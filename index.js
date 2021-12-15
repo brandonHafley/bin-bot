c95b7f7b323042fc391bd72816fbeca0a2348287
brandonHafley <bchafley@gmail.com>
2021-12-15 11:52:26 -0600

    Add command and event handlers

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+const { Client, Intents, Collection } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS]
 });
 
-client.once('ready', () => {
-    console.log('Bin-Bot is ready to rock n\' roll!');
-});
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-
-    const { commandName } = interaction;
-    if (commandName === 'ping') {
-        await interaction.reply('Pong!');
-    } else if (commandName === 'server') {
-        await interaction.reply(
-            `Server name : ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nServer created: ${interaction.guild.createdAt}`
-        );
-    } else if (commandName === 'user') {
-        await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args, client.commands));
     }
-});
+}
 
 client.login(token);
